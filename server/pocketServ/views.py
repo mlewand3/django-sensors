8eea838ca6f9fba7201da2c4d3b17e48f48eecb0
Peter Novotnak <yellobes@gmail.com>
2013-05-19 22:53:50 -0500

    working on cron job

--- server/pocketServ/views.py
+++ server/pocketServ/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import HttpResponse
-from django.core import serializers
 
 from sensors.models import *
 
 import json
+import glob
+import re
 
 
 def home(request):
