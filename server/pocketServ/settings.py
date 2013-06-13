4ee8e59299c2aa3ed67231a850541b1e02909140
Peter Novotnak <yellobes@gmail.com>
2013-06-12 20:58:58 -0500

    added grappelli

--- server/pocketServ/settings.py
+++ server/pocketServ/settings.py
@@ -110,7 +110,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
+    'grappelli',
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
