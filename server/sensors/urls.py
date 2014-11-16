c2d670ff19e985c66f407caa5263b6817e153aa8
Peter Novotnak <pnovotnak@flexion.us>
2014-11-15 16:18:48 -0800

    renamed update url

--- server/sensors/urls.py
+++ server/sensors/urls.py
@@ -5,7 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'sensors.views.home', name='home'),
-    url(r'^update-sensors/?$', 'sensors.views.update_sensors', name='update_sensors'),
+    url(r'^update-readings/?$', 'sensors.views.update_sensors', name='update_readings'),
     url(r'^ajax-gauge-update/?$', 'sensors.views.ajax_gauge_update', name='ajax_gauge_update'),
 
     url(r'^admin/', include(admin.site.urls)),
